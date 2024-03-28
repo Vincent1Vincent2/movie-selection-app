04ce687f720d957ab4e8e2231bb86e486fb25a19
Vincent <Vincent.ahlin99@gmail.com>
2024-03-28 10:25:54 +0100

    Installing icons library, UI components library. Added icons to layout

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Movie } from "@/data/types";
+import { Search } from "lucide-react";
 import Link from "next/link";
 import { useState } from "react";
 import { useMovies } from "../context/movieContext";
@@ -17,23 +18,38 @@ export default function SearchBar() {
   const handleOptionClick = (movie: Movie) => {
     setSearchQuery(movie.title);
     setIsOpen(false);
+    // Clear the input field
+    setSearchQuery("");
   };
 
   const handleDropdownClick = () => {
     setIsOpen(false);
   };
 
+  const handleSearchIconClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="relative">
-      <input
-        type="text"
-        placeholder="Search movies..."
-        value={searchQuery}
-        onChange={handleInputChange}
-        onFocus={() => setIsOpen(true)}
-        onBlur={() => setIsOpen(false)}
-        className="w-full p-2 border rounded"
-      />
+      {isOpen ? (
+        <input
+          type="text"
+          placeholder="Search movies..."
+          value={searchQuery}
+          onChange={handleInputChange}
+          onFocus={() => setIsOpen(true)}
+          onBlur={() => setIsOpen(false)}
+          className="w-full p-2 border rounded"
+        />
+      ) : (
+        <div
+          className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+          onClick={handleSearchIconClick}
+        >
+          <Search />
+        </div>
+      )}
       {(searchQuery.length > 0 || isOpen) && (
         <div className="absolute z-10 w-full bg-white border rounded shadow-lg">
           {filteredMovies.map((movie) => (
