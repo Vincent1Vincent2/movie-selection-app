b18453acaedbdcb5924111028f30eac96b1498a9
Vincent <Vincent.ahlin99@gmail.com>
2024-03-30 20:21:11 +0100

    Both accordion and carousel on home page, rest of the pages has carousel. Also made responsive

--- src/app/bookmarks/page.tsx
+++ src/app/bookmarks/page.tsx
@@ -1,27 +1,24 @@
 "use client";
 
 import MovieCard from "../components/MovieCard";
-import { MovieCarousel } from "../components/MovieCarousel";
 import { useMovies } from "../context/movieContext";
 
 export default function Bookmarks() {
   const { favorites } = useMovies();
 
   return (
-    <div className="flex flex-col mx-8">
-      <h1 className=" font-bold text-2xl py-5 max-sm:flex max-sm:justify-center">
+    <div className="flex flex-col">
+      <h1 className="mx-8 font-bold text-2xl py-5 max-sm:flex max-sm:justify-center">
         Your Favorites
       </h1>
       {favorites.length === 0 ? (
         <p>You haven't added any movies to your favorites yet.</p>
       ) : (
-        <MovieCarousel>
+        <div className="flex flex-wrap gap-10 mx-5 max-sm:justify-center max-sm:items-center">
           {favorites.map((movie) => (
-            <div key={movie.title} className="embla__slide">
-              <MovieCard movie={movie} />
-            </div>
+            <MovieCard key={movie.title} movie={movie} />
           ))}
-        </MovieCarousel>
+        </div>
       )}
     </div>
   );
