1543bc7d759824cbb22916472d0eed8455106853
Vincent <Vincent.ahlin99@gmail.com>
2024-03-30 17:14:45 +0100

    Favorites page styled

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -7,12 +7,14 @@ export default function Bookmarks() {
   const { favorites } = useMovies();
 
   return (
-    <div>
-      <h1>Favorites</h1>
+    <div className="flex flex-col">
+      <h1 className="mx-8 font-bold text-2xl py-5 max-sm:flex max-sm:justify-center">
+        Your Favorites
+      </h1>
       {favorites.length === 0 ? (
         <p>You haven't added any movies to your favorites yet.</p>
       ) : (
-        <div>
+        <div className="flex flex-wrap gap-10 mx-5 max-sm:justify-center max-sm:items-center">
           {favorites.map((movie) => (
             <MovieCard movie={movie} />
           ))}
