20c8bb8f22b6bedd159f423acb8c6ffcb372392b
Vincent <Vincent.ahlin99@gmail.com>
2024-03-30 18:04:48 +0100

    Accordion and animations added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,72 @@
-import RecommendedMovies from "./recommended/page";
+"use client";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@radix-ui/react-accordion";
+import { ArrowDownCircle } from "lucide-react";
+import RecommendedMoviesCard from "./recommended/recommendedMovies";
 import StartMovies from "./startMovies/page";
 import TrendingMoviesCard from "./trending/trendingMovies";
 
 export default function Home() {
   return (
     <main className="flex flex-col gap-10">
-      <div>
-        <h2 className="mx-8 font-bold text-2xl py-5 max-sm:flex max-sm:justify-center">
-          Must See Movies
-        </h2>
-        <StartMovies />
-      </div>
-      <div>
-        <h2 className="mx-8 font-bold text-2xl py-5">Trending Movies</h2>
-        <TrendingMoviesCard />
-      </div>
-      <div>
-        <RecommendedMovies />
-      </div>
+      <Accordion
+        type="single"
+        defaultValue="item-1"
+        collapsible
+        className="w-full"
+      >
+        <AccordionItem value="item-1">
+          <div>
+            <div className="flex justify-between">
+              <h2 className="mx-8 font-bold text-2xl py-5 max-sm:flex max-sm:justify-center">
+                Must See Movies
+              </h2>
+              <AccordionTrigger className="AccordionTrigger mr-40">
+                <ArrowDownCircle className="AccordionArrow" />
+              </AccordionTrigger>
+            </div>
+            <AccordionContent className="AccordionContent">
+              <StartMovies />
+            </AccordionContent>
+          </div>
+        </AccordionItem>
+
+        <AccordionItem value="item-2">
+          <div>
+            <div className="flex justify-between">
+              <h2 className="mx-8 font-bold text-2xl py-5 max-sm:flex max-sm:justify-center">
+                Trending Movies
+              </h2>
+              <AccordionTrigger className="AccordionTrigger mr-40">
+                <ArrowDownCircle className="AccordionArrow" />
+              </AccordionTrigger>
+            </div>
+            <AccordionContent className="AccordionContent">
+              <TrendingMoviesCard />
+            </AccordionContent>
+          </div>
+        </AccordionItem>
+        <AccordionItem value="item-3">
+          <div className="flex justify-between">
+            <h2 className="mx-8 font-bold text-2xl py-5 max-sm:flex max-sm:justify-center">
+              Your Recommendations
+            </h2>
+            <AccordionTrigger className="AccordionTrigger mr-40">
+              <ArrowDownCircle className="AccordionArrow" />
+            </AccordionTrigger>
+          </div>
+          <AccordionContent className="AccordionContent">
+            <div>
+              <RecommendedMoviesCard />
+            </div>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </main>
   );
 }
